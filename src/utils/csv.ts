fc50def062e13563e1717423450da47679664604
zeeshan <zeeshanweb.official@gmail.com>
2024-10-26 02:41:15 +0500

    required changes

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -1,13 +1,21 @@
 import UserSessionCards, { IUserSessionCards } from '../models/userSessionCards';
 
 
+const normalizeCardName = (cardName: string) => {
+    // Remove the file extension (.png)
+    const withoutExtension = cardName.replace('.png', '');
+    // Replace the underscore (_) with a space and capitalize the suit
+    const formattedName = withoutExtension.replace('_', ' ').replace(/\b\w/g, char => char.toUpperCase());
+    return formattedName
+
+}
 export function createCsvString(userSessionCards: IUserSessionCards[]): string {
     // Define the CSV header row
     const headerRow = ['Name', 'Email', 'Card'];
 
     // Create a CSV string by iterating over userSessionCards and appending rows
     const csvRows = userSessionCards.map(card => {
-        return `${card.userId.name} ${card.userId.email},${card.card}`;
+        return `${normalizeCardName(card.userId.name)} ${card.userId.email},${card.card}`;
     });
 
     const csvString = headerRow.join(',') + '\n' + csvRows.join('\n');
